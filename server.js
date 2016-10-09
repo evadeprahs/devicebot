3a08ef3962f972f10c98fec368f8445f66146106
isobar-dsharpe <dave.sharpe@isobar.com>
2016-10-09 17:39:32 +1100

    pulled example from mbrevoort's slapp demo

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 'use strict'
 const express = require('express')
-const Slapp = require('Slapp')
-
+const Slapp = require('slapp')
+const BeepBoopConvoStore = require('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
-var slapp = Slapp({ context: BeepBoopContext() })
+var slapp = Slapp({
+  convo_store: BeepBoopConvoStore(),
+  context: BeepBoopContext()
+})
 
-// attach handlers to an Express app
 var app = slapp.attachToExpress(express())
 
-slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
-  msg
-    .say(`${greeting}, how are you?`)
-    .route('handleHowAreYou')  // where to route the next msg in the conversation
+slapp.message('hi (.*)', ['direct_message'], (msg, text, match1) => {
+  msg.say('How are you?').route('handleHi', { what: match1 })
 })
 
-// register a route handler
-slapp.route('handleHowAreYou', (msg) => {
-  // respond with a random entry from array
-  msg.say(['Me too', 'Noted', 'That is interesting'])
+slapp.route('handleHi', (msg, state) => {
+  msg.say(':smile: ' + state.what)
 })
 
-console.log('Listening on: ' + process.env.PORT)
-app.listen(process.env.PORT) 
+app.get('/', function (req, res) {
+  res.send('Hello')
+})
+
+console.log('Listening on :' + process.env.PORT)
+app.listen(process.env.PORT)
